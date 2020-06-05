53573f103554988f0895ff1b465b192c39e6cd7a
Carly Kelly <carlyfkelly@gmail.com>
2020-06-05 19:02:04 -0400

    iterrows made efficient

--- Climate_Analysis/Climate.py
+++ Climate_Analysis/Climate.py
@@ -38,23 +38,23 @@ def precipitation():
     one_year_ago = pd.read_sql(f"SELECT DATE('{most_recent}','-1 year') AS Date LIMIT 1", conn)
     one_year_ago = one_year_ago['Date'][0]
     precip_df = pd.read_sql(f"SELECT date, prcp FROM Measurement WHERE date >='{one_year_ago}' AND date <='{most_recent}' ORDER BY date", conn)
+    precip_df = precip_df.groupby('date').mean()
+    return pd.Series(precip_df['prcp'],precip_df.index).to_json()
+    
     ### Putting the results into a dictionary where date is the key and prcp is value
-    precip_dict = {}
-    for index, row in precip_df.iterrows():
-        precip_dict[row[0]]=[row[1]]
-    precip_df = pd.DataFrame(precip_dict)
-    precip_json = precip_df.to_json(orient = 'records')   
-    return (precip_json)
+    #precip_dict = {}
+    #for index, row in precip_df.iterrows():
+        #precip_dict[row[0]]=[row[1]]
+    #precip_df = pd.DataFrame(precip_dict)
+    #precip_json = precip_df.to_json(orient = 'records')   
+    #return (precip_json)
 #%%
 @app.route("/api/v1.0/stations")
 def stations():
     conn = engine.connect()
     station_df = pd.read_sql("SELECT name, station FROM station",conn)
-    ##turning results into a list of stations
-    stations_list = []
-    for index, row in station_df.iterrows():
-        stations_list.append(row[0])
-    return jsonify(stations_list)
+    station_list = list(station_df['name'])
+    return jsonify(station_list)
 
 
 #%%
@@ -67,12 +67,10 @@ def tobs():
     one_year_ago = pd.read_sql(f"SELECT DATE('{most_recent}','-1 year') AS Date LIMIT 1", conn)
     one_year_ago = one_year_ago['Date'][0]
     tob_df = pd.read_sql(f"SELECT date, tobs AS temperature FROM measurement WHERE date >='{one_year_ago}' AND date <='{most_recent}'", conn)
+    tob_list = list(tob_df['temperature'])
     #tob_json = tob_df.to_json(orient='records')
-    #Creating just a list of temperatures
-    tobs_list = []
-    for index,row in tob_df.iterrows():
-        tobs_list.append(row[1])
-    return jsonify(tobs_list)
+
+    return jsonify(tob_list)
 #%%
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start,end):
@@ -81,12 +79,10 @@ def start_end(start,end):
     end_fix = end.replace("/","-")
     temp_range = pd.read_sql(f"SELECT MIN(tobs) AS minimum_temperature, AVG(tobs) AS average_temperature, MAX(tobs) AS maximum_temperature FROM measurement WHERE date >='{start_fix}' AND date <='{end_fix}'", conn)
     #temp_range_json = temp_range.to_json(orient='records')
-    #Creating a lits of the three temp info
     start_end_list = []
-    for index,row in temp_range.iterrows():
-        start_end_list.append(row[0])
-        start_end_list.append(row[1])
-        start_end_list.append(row[2])
+    start_end_list.append(temp_range['minimum_temperature'][0])
+    start_end_list.append(temp_range['average_temperature'][0])
+    start_end_list.append(temp_range['maximum_temperature'][0])
     return jsonify(start_end_list)
 #%%
 
@@ -96,12 +92,10 @@ def start(start):
     start_fix = start.replace("/", "-")
     temp_ranges = pd.read_sql(f"SELECT MIN(tobs) AS minimum_temperature, AVG(tobs) AS average_temperature, MAX(tobs) AS maximum_temperature FROM measurement WHERE date >='{start_fix}'", conn)
     #temp_range_json = temp_range.to_json(orient='records')
-    #Creating a list of the three temp info
     start_list = []
-    for index,row in temp_ranges.iterrows():
-        start_list.append(row[0])
-        start_list.append(row[1])
-        start_list.append(row[2])
+    start_list.append(temp_ranges['minimum_temperature'][0])
+    start_list.append(temp_ranges['average_temperature'][0])
+    start_list.append(temp_ranges['maximum_temperature'][0])
     return jsonify(start_list)
     
 
